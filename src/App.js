aed010ae93d23eff70f01ec2f02f499b0ddbf711
sajib <me.mrsajib@gmail.com>
2022-04-10 16:26:58 +0600

    refactor componets and navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
 import Header from "./components/Header/Header";
-import Details from './components/Details/Details';
+import Details from "./components/Details/Details";
+import LogIn from "./components/Login/LogIn";
+import SignUp from "./components/SignUp/SignUp";
 
 function App() {
   return (
-    <div className="App">
-     <Header />
+    <div>
+      <Header />
       <Details></Details>
-  
+      <LogIn />
+      <SignUp />
     </div>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div>
-      <h1>nav item</h1>
+    <div className="nav-wrap">
+      <div className="nav-container container">
+        <div className="logo">logo</div>
+        <div className="nav-item">
+          <nav>
+            <a href="/">home</a>
+            <a href="/shop">Shop</a>
+            <a href="/orders">Orders</a>
+            <a href="/login">Login</a>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 };
